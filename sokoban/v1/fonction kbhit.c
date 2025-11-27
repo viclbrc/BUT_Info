c002eb8745d775430c0f7a2af4cfa10c6130a152
Vic_ <160769384+viclbrc@users.noreply.github.com>
2025-11-27 14:32:30 +0100

    Add files via upload

---
+++ sokoban/v1/fonction kbhit.c
@@ -0,0 +1,30 @@
+int kbhit(){
+	// la fonction retourne :
+	// 1 si un caractere est present
+	// 0 si pas de caractere présent
+	int unCaractere=0;
+	struct termios oldt, newt;
+	int ch;
+	int oldf;
+
+	// mettre le terminal en mode non bloquant
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+ 
+	ch = getchar();
+
+	// restaurer le mode du terminal
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	fcntl(STDIN_FILENO, F_SETFL, oldf);
+ 
+	if(ch != EOF){
+		ungetc(ch, stdin);
+		unCaractere=1;
+	} 
+	return unCaractere;
+}
+
---
+++ sokoban/v1/procedure Charger et Enregistrer.c
@@ -0,0 +1,34 @@
+void chargerPartie(t_Plateau plateau, char fichier[]){
+    FILE * f;
+    char finDeLigne;
+
+    f = fopen(fichier, "r");
+    if (f==NULL){
+        printf("ERREUR SUR FICHIER");
+        exit(EXIT_FAILURE);
+    } else {
+        for (int ligne=0 ; ligne<TAILLE ; ligne++){
+            for (int colonne=0 ; colonne<TAILLE ; colonne++){
+                fread(&plateau[ligne][colonne], sizeof(char), 1, f);
+            }
+            fread(&finDeLigne, sizeof(char), 1, f);
+        }
+        fclose(f);
+    }
+}
+    
+
+
+void enregistrerPartie(t_Plateau plateau, char fichier[]){
+    FILE * f;
+    char finDeLigne='\n';
+
+    f = fopen(fichier, "w");
+    for (int ligne=0 ; ligne<TAILLE ; ligne++){
+        for (int colonne=0 ; colonne<TAILLE ; colonne++){
+            fwrite(&plateau[ligne][colonne], sizeof(char), 1, f);
+        }
+        fwrite(&finDeLigne, sizeof(char), 1, f);
+    }
+    fclose(f);
+}

aca1aa507910797f49884704abd244db7b255448
viclbrc <lebroc.v@gmail.com>
2026-01-26 18:31:23 +0100

    tp 2

--- BUT1/Java/TP2/TestStock.java
+++ BUT1/Java/TP2/TestStock.java
@@ -2,28 +2,26 @@ import java.util.*;
 
 public class TestStock {
     public static void main(String[] args) {
-        Produit prod = new Produit(5);
-        prod.afficher();
-        Pile pile = new Pile(10);
-        pile.pileVide();
-        pile.pilePleine();
-        pile.empiler(prod);
-        pile.depiler();
-        pile.sommet();
-        pile.afficherStock();
+        Stock stock = new Stock(20);
+        stock.afficheMenu();
     }
 }
 
 class Produit {
     private String reference;
-    private Integer date_entree;
-    private Scanner ref = new Scanner(System.in);
+    private int date_entree;
 
-    public Produit(int date_entree) {
+    public Produit(String reference, int date_entree) {
+        this.reference = reference;
         this.date_entree = date_entree;
-        System.out.println("Référence du produit : ");
-        reference = this.ref.nextLine();
-        ref.close();
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public int getDateEntree() {
+        return date_entree;
     }
 
     public String toString() {
@@ -36,39 +34,162 @@ class Produit {
 }
 
 class Pile {
-    private ArrayList tab_prod;
-    private static int indice;
+    private Produit[] tab_prod;
+    private int indice;
 
     public Pile(int max) {
-        tab_prod = new ArrayList(max);
-        indice = max;
+        tab_prod = new Produit[max];
+        indice = -1;
     }
 
     public boolean pileVide() {
-        tab_prod.isEmpty();
+        return indice == -1;
     }
 
     public boolean pilePleine() {
-        return this.tab_prod.size() == indice;
+        return indice == tab_prod.length - 1;
     }
 
     public void empiler(Produit p) {
-        tab_prod.add(indice, p);
+        if (!pilePleine()) {
+            indice++;
+            tab_prod[indice] = p;
+        } else {
+            System.out.println("Pile pleine !");
+        }
     }
 
     public void depiler() {
-        tab_prod.remove(indice);
+        if (!pileVide()) {
+            tab_prod[indice] = null;
+            indice--;
+        } else {
+            System.out.println("Pile vide !");
+        }
     }
 
     public Produit sommet() {
-        return tab_prod.get(indice);
+        if (!pileVide()) {
+            return tab_prod[indice];
+        }
+        return null;
     }
 
     public String toString() {
-        return ("Stock : " + this.tab_prod);
+        StringBuilder sb = new StringBuilder("Stock : [");
+        for (int i = 0; i <= indice; i++) {
+            sb.append(tab_prod[i].toString());
+            if (i < indice) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
     public void afficherStock() {
-        System.out.println(this.toString());
+        if (pileVide()) {
+            System.out.println("Stock vide");
+        } else {
+            System.out.println(this.toString());
+        }
+    }
+}
+
+class Stock {
+    private Pile pile;
+    private int dateJour;
+    private Scanner scanner;
+
+    public Stock(int taille) {
+        pile = new Pile(taille);
+        dateJour = 1;
+        scanner = new Scanner(System.in);
+    }
+
+    public void entrer(Produit p) {
+        if (!pile.pilePleine()) {
+            pile.empiler(p);
+            System.out.println("Produit " + p.getReference() + " ajouté au stock");
+            dateJour++;
+        } else {
+            System.out.println("Stock plein !");
+        }
+    }
+
+    public void sortir(int dateJ) {
+        if (pile.pileVide()) {
+            System.out.println("Stock vide !");
+            return;
+        }
+
+        Produit premier = pile.sommet();
+        
+        // Retirer les produits périmés successivement
+        while (!pile.pileVide()) {
+            Produit p = pile.sommet();
+            int anciennete = dateJ - p.getDateEntree();
+            
+            if (anciennete > 5) {
+                // Produit périmé, le retirer
+                pile.depiler();
+            } else {
+                // Produit frais trouvé, le retirer et terminer
+                pile.depiler();
+                System.out.println("Produit " + p.getReference() + " sorti du stock");
+                return;
+            }
+        }
+        
+        // Si on arrive ici, tous les produits étaient périmés
+        System.out.println("Produit " + premier.getReference() + " périmé, stock intégralement supprimé");
+    }
+
+    public void afficheMenu() {
+        boolean continuer = true;
+
+        while (continuer) {
+            System.out.println("\n--- MENU ---");
+            System.out.println("e : entrée d'un produit dans le stock");
+            System.out.println("s : sortie d'un produit du stock");
+            System.out.println("i : incrémenter la date du jour");
+            System.out.println("a : afficher le stock");
+            System.out.println("q : quitter");
+            System.out.print("Choisissez une option : ");
+
+            String choix = scanner.nextLine().trim();
+
+            switch (choix) {
+                case "e":
+                    System.out.print("Référence du produit : ");
+                    String reference = scanner.nextLine();
+                    Produit produit = new Produit(reference, dateJour);
+                    entrer(produit);
+                    break;
+
+                case "s":
+                    sortir(dateJour);
+                    break;
+
+                case "i":
+                    dateJour++;
+                    System.out.println("Date du jour incrémentée à : " + dateJour);
+                    break;
+
+                case "a":
+                    System.out.println("Date du jour : " + dateJour);
+                    pile.afficherStock();
+                    break;
+
+                case "q":
+                    continuer = false;
+                    break;
+
+                default:
+                    System.out.println("Option invalide !");
+            }
+        }
+
+        scanner.close();
     }
 }
